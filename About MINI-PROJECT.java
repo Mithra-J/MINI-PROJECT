6a8a601bafa264722b3cb5bcea930a6afba16fa6
Mithra J <mithraacct@gmail.com>
2024-11-07 23:08:03 +0530

    Create About MINI-PROJECT.java

---
+++ About MINI-PROJECT.java
@@ -0,0 +1,4 @@
+I've created a rock, paper and scissors game using Java.
+This development equipped me with java basics and string concepts.
+No matter how old we're our love towards CODING and GAMES never end.
+Check my code , play and HAVE FUN!!
